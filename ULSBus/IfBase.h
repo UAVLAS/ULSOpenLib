2637fae8803c00871b601c6211bad3b27dfc9e14
Yury Kapacheuski <kapacheuski@gmail.com>
2020-10-28 13:51:52 +0300

    Ifbase Added

---
+++ ULSBus/IfBase.h
@@ -0,0 +1,42 @@
+#ifndef IFBASE_H
+#define IFBASE_H
+#include<inttypes.h>
+
+typedef struct
+{
+    uint8_t*  buf;
+    uint32_t  lenght;
+}_if_buffer_instance;
+
+class IfBase
+{
+public:
+    IfBase()
+    {
+
+    };
+    virtual void open(){};
+    virtual bool send(_if_buffer_instance *ifBufferInstace){(void)ifBufferInstace;return false;};
+    virtual uint32_t maxTxPacketLenght(){return 0;}
+    bool send(uint8_t *buf, uint32_t lenght)
+    {
+        txBufInstance.buf = buf;
+        txBufInstance.lenght = lenght;
+        return send(&txBufInstance);
+    };
+    bool send()
+    {
+      return send(&txBufInstance);
+    }
+    _if_buffer_instance rxBufInstance;
+    _if_buffer_instance txBufInstance;
+    virtual void enableEscIfSupprted(bool esc)
+    {
+        (void)esc;
+    }
+protected:
+};
+
+
+
+#endif // IFBASE_H
