ae3129235f88b20e5428501ee01bec248d7a7590
Yury Kapacheuski <kapacheuski@gmail.com>
2020-10-29 17:09:34 +0300

    Devices updated

--- ULSBus/Library/ULSDeviceBase.cpp
+++ ULSBus/Library/ULSDeviceBase.cpp
@@ -3,10 +3,10 @@
 
 ULSDeviceBase::ULSDeviceBase(uint8_t selfId,uint8_t remoteId,uint16_t devClass,uint16_t hardware):
 ULSBusObjectsDictionary(selfId,remoteId,devClass,hardware),
-  signature(0x0001,"Device signature data",ULSBUS_OBJECT_PERMITION_SYSCONFIG),
-  sys_cmd(0x0010,"Device command",ULSBUS_OBJECT_PERMITION_WRITEONLY),
-  sys_status(0x0011,"Device status data",ULSBUS_OBJECT_PERMITION_READONLY),
-  sys_flash(0x0100,"Device flash page",ULSBUS_OBJECT_PERMITION_WRITEONLY)
+  signature(0x0001,"[Device/Signature]Device signature data",ULSBUS_OBJECT_PERMITION_SYSCONFIG),
+  sys_cmd(0x0010,"[Device/Command]Device command",ULSBUS_OBJECT_PERMITION_WRITEONLY),
+  sys_status(0x0011,"[Device/Status]Device status data",ULSBUS_OBJECT_PERMITION_READONLY),
+  sys_flash(0x0100,"[Device/Flash]Device flash page",ULSBUS_OBJECT_PERMITION_WRITEONLY)
 {
     add(&signature);
     add(&sys_cmd);
--- ULSBus/Library/ULSDevice_ULSQR1.cpp
+++ ULSBus/Library/ULSDevice_ULSQR1.cpp
@@ -2,11 +2,11 @@
 
 ULSDevice_ULSQR1_R1::ULSDevice_ULSQR1_R1(uint8_t selfId,uint8_t remoteId):
 ULSDeviceBase(selfId,remoteId,__DEVICE_CLASS_ULSQR1,__DEVICE_HW_ULSQR1_R1),
-  status(0x0001,"Device signature data",ULSBUS_OBJECT_PERMITION_SYSCONFIG),
-  array2000(0x5101,"Test array 2000",ULSBUS_OBJECT_PERMITION_READWRITE),
-  array2048(0x5102,"Test array 2048",ULSBUS_OBJECT_PERMITION_READWRITE),
-  tcfgA(0x5001,"Description 1",ULSBUS_OBJECT_PERMITION_READWRITE),
-  tcfg(0x5002,"Description 2",ULSBUS_OBJECT_PERMITION_READWRITE)
+  status(0x0001,"[Device/Status]Device status data",ULSBUS_OBJECT_PERMITION_SYSCONFIG),
+  array2000(0x5101,"[Test/Array1]Test array 2000",ULSBUS_OBJECT_PERMITION_READWRITE),
+  array2048(0x5102,"[Test/Array2]Test array 2048",ULSBUS_OBJECT_PERMITION_READWRITE),
+  tcfgA(0x5001,"[Config/tmp1]Config temp",ULSBUS_OBJECT_PERMITION_READWRITE),
+  tcfg(0x5002,"[Config/tmp2]Config temp2",ULSBUS_OBJECT_PERMITION_READWRITE)
 {
     add(&status);
     add(&array2000);
