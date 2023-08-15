d43e53f6eb9926932245dd1064ca078e0d5d6f31
kapacheuski <kapacheuski@gmail.com>
2023-08-15 11:50:40 +0300

    Some debug fixes

--- utils/udebug.h
+++ utils/udebug.h
@@ -21,11 +21,15 @@ void uDebugPacket(const char* msg,const char* msg2, uint8_t *buf,uint32_t len);
 #ifdef ULS_DEBUG
 #define DEBUG_MSG(MSG,...) uDebug(MSG,##__VA_ARGS__)
 #define DEBUG_ERROR(FILE,LINE,MSG,...) uError(FILE,LINE,MSG,##__VA_ARGS__)
-#define DEBUG_PACKET(MSG,MSG2,BUF,LEN) uDebugPacket(MSG,MSG2,BUF,LEN)
 
 #else
 #define DEBUG_MSG(MSG,...) (void)MSG;
 #define DEBUG_ERROR(FILE,LINE,MSG,...) {(void)FILE;(void)LINE;(void)MSG;}
+#endif
+
+#ifdef ULS_DEBUG_FULL
+#define DEBUG_PACKET(MSG,MSG2,BUF,LEN) uDebugPacket(MSG,MSG2,BUF,LEN)
+#else
 #define DEBUG_PACKET(MSG,MSG2,BUF,LEN) {(void)MSG;(void)MSG2;(void)BUF;(void)LEN;}
 #endif
 
